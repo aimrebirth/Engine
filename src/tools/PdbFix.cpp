087d83cc09de11063cb19364b64e29549a06b203
lzwdgc <lzwdgc@gmail.com>
2015-04-15 19:26:02 +0300

    Update PdbFix.

--- src/tools/PdbFix.cpp
+++ src/tools/PdbFix.cpp
@@ -8,9 +8,9 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if (argc != 2)
     {
-        printf("Usage: %s file.exe new.pdb\n", argv[0]);
+        printf("Usage: %s file.exe", argv[0]);
         return 1;
     }
     
@@ -20,23 +20,53 @@ int main(int argc, char *argv[])
     boost::system::error_code ec;
     uint64_t size = boost::filesystem::file_size(p, ec);
     if (ec)
-        return 1;
+        return 2;
     FILE *f = fopen(argv[1], "rb");
     if (!f)
-        return 1;
+        return 3;
     std::vector<uint8_t> file(size, 0);
     fread(file.data(), 1, size, f);
     fclose(f);
 
-    PIMAGE_DEBUG_DIRECTORY pDebugDesc = NULL;
-	pDebugDesc = (PIMAGE_DEBUG_DIRECTORY)ImageDirectoryEntryToData(file.data(), FALSE, IMAGE_DIRECTORY_ENTRY_DEBUG, &ulSize);
-    
-    uint8_t *ptr = file.data() + pDebugDesc->PointerToRawData + 0x18;
-    memcpy(ptr, argv[2], strlen(argv[2]) + 1);
+    // dll
+    uint8_t *ptr = file.data();
+    int offset = -0x20;
 
+    PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)ptr;
+    PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)(ptr + dos->e_lfanew);
+    int addr = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+
+    PIMAGE_EXPORT_DIRECTORY pExportDesc = NULL;
+	pExportDesc = (PIMAGE_EXPORT_DIRECTORY)ImageDirectoryEntryToData(file.data(), FALSE, IMAGE_DIRECTORY_ENTRY_EXPORT, &ulSize);
+
+    int start = (uint8_t *)pExportDesc - ptr;
+    int diff = pExportDesc->Name - addr;
+    int real_diff = pExportDesc->Name - (start + diff);
+
+    ptr += start + offset;
+    memcpy(
+        ptr,
+        p.filename().string().c_str(),
+        p.filename().string().size());
+    
+    ptr = file.data() + pExportDesc->Name - real_diff;
+    memcpy(ptr, "test", strlen("test") + 1);
+
+    pExportDesc->Name = start + offset + real_diff;
+
+    // pdb
+    PIMAGE_DEBUG_DIRECTORY pDebugDesc = NULL;
+    pDebugDesc = (PIMAGE_DEBUG_DIRECTORY)ImageDirectoryEntryToData(file.data(), FALSE, IMAGE_DIRECTORY_ENTRY_DEBUG, &ulSize);
+
+    std::string pdb = p.filename().stem().string() + ".pdb";
+
+    ptr = file.data() + pDebugDesc->PointerToRawData + 0x18;
+    memcpy(ptr, pdb.c_str(), pdb.size() + 1);
+
+    // write
     f = fopen(argv[1], "wb");
     if (!f)
-        return 1;
+        return 4;
     fwrite(file.data(), 1, size, f);
     fclose(f);
 
