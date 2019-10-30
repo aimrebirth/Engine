a3652d49a3c117223c74b743f236086da9a1d576
lzwdgc <lzwdgc@gmail.com>
2019-10-30 03:09:52 +0300

    Update sw script.

--- sw.cpp
+++ sw.cpp
@@ -3,6 +3,7 @@
 
 void configure(Build &b)
 {
+#ifndef SW_CPP_DRIVER_API_VERSION
     auto ss = b.createSettings();
     ss.Native.LibrariesType = LibraryType::Static;
     ss.Native.ConfigurationType = ConfigurationType::ReleaseWithDebugInformation;
@@ -100,6 +101,7 @@ void configure(Build &b)
                 write_file(b.BinaryDir / ("sqlite3_" + toString(nt.getSettings().Native.ConfigurationType) + ".txt"), nt.getImportLibrary().u8string());
         }
     });
+#endif
 }
 
 void build(Solution &s)
@@ -107,36 +109,44 @@ void build(Solution &s)
     auto &Engine = s.addSharedLibrary("Polygon4.Engine", "master");
 
     auto &logger = Engine.addStaticLibrary("logger");
-    logger.CPPVersion = CPPLanguageStandard::CPP17;
-    logger.setRootDirectory("src/tools");
-    logger += "Logger.*"_rr;
-    logger.Public += "org.sw.demo.boost.log-1"_dep;
-    logger.Public += "USE_LOGGER"_def;
-
-    Engine.ApiName = "P4_ENGINE_API";
-    Engine.CPPVersion = CPPLanguageStandard::CPP17;
-    Engine += "include/Polygon4/.*"_rr;
-    Engine += "src/.*"_r;
-    Engine += "src/tools/Hotpatch.*"_rr;
-
-    Engine.Public += logger,
-        "pub.egorpugin.primitives.executor-master"_dep,
-        "pub.egorpugin.primitives.command-master"_dep,
-        "pub.lzwdgc.polygon4.datamanager-master"_dep,
-        "org.sw.demo.lua-5"_dep
-        ;
-    Engine += "dbghelp.lib"_slib;
+    {
+        logger.CPPVersion = CPPLanguageStandard::CPP17;
+        logger.setRootDirectory("src/tools");
+        logger += "Logger.*"_rr;
+        logger.Public += "org.sw.demo.boost.log"_dep;
+        logger.Public += "USE_LOGGER"_def;
+    }
 
     {
-        String prefix = "ScriptAPI";
-        auto c = Engine.addCommand();
-        c << cmd::prog("org.sw.demo.swig"_dep)
-            << "-c++"
-            << "-lua"
-            << "-o"
-            << cmd::out(prefix + "_lua.cpp", cmd::Skip)
-            << cmd::in("src/" + prefix + ".i")
+        Engine.ApiName = "P4_ENGINE_API";
+        Engine.CPPVersion = CPPLanguageStandard::CPP17;
+        Engine += "include/Polygon4/.*"_rr;
+        Engine += "src/.*"_r;
+        Engine += "src/tools/Hotpatch.*"_rr;
+
+        auto d = Engine.Public + "pub.lzwdgc.polygon4.datamanager.memory-master"_dep;
+        d->getOptions()["alligned-allocator"] = "1";
+        d->getOptions()["alligned-allocator"].setRequired();
+
+        Engine.Public += logger,
+            "pub.egorpugin.primitives.executor-master"_dep,
+            "pub.egorpugin.primitives.command-master"_dep,
+            "pub.lzwdgc.polygon4.datamanager-master"_dep,
+            "org.sw.demo.lua"_dep
             ;
+        Engine += "dbghelp.lib"_slib;
+
+        {
+            String prefix = "ScriptAPI";
+            auto c = Engine.addCommand();
+            c << cmd::prog("org.sw.demo.swig"_dep)
+                << "-c++"
+                << "-lua"
+                << "-o"
+                << cmd::out(prefix + "_lua.cpp", cmd::Skip)
+                << cmd::in("src/" + prefix + ".i")
+                ;
+        }
     }
 
     auto &pdbfix = Engine.addExecutable("tools.pdbfix");
